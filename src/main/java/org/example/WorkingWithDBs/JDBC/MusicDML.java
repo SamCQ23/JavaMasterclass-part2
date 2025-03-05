4fa8c448403281e67d0e33c364544c672ecc3e15
samuel.clarke-quy <samuel.clarke-quy@ten10.com>
2025-03-05 09:38:53 +0000

    switching to section_22 branch

--- src/main/java/org/example/WorkingWithDBs/JDBC/MusicDML.java
+++ src/main/java/org/example/WorkingWithDBs/JDBC/MusicDML.java
@@ -172,6 +172,18 @@ public class MusicDML {
             throws SQLException {
 
         System.out.println("AUTOCOMMIT = " + conn.getAutoCommit());
+        String deleteSongs = """
+                DELETE FROM music.songs WHERE album_id =
+                (SELECT ALBUM_ID from music.albums WHERE album_name = '%s')"""
+                .formatted(albumName);
+        int deletedSongs = statement.executeUpdate(deleteSongs);
+        System.out.printf("Deleted %d rows from music.songs%n", deletedSongs);
+        String deleteAlbums = "DELETE FROM music.albums WHERE album_name='%s'"
+                .formatted(albumName);
+        int deletedAlbums = statement.executeUpdate(deleteAlbums);
+        System.out.printf("Deleted %d rows from music.albums%n",
+                deletedAlbums);
+
     }
 
 }
